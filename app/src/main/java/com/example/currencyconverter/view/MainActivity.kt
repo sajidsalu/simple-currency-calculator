7c1ceccf0e953510459d5eaaf7fef16c25090418
sajid.m@experionglobal.com <sajid.m@experionglobal.com>
2022-06-06 18:04:46 +0530

    calculator basic ui

---
+++ app/src/main/java/com/example/currencyconverter/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.currencyconverter.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.currencyconverter.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
